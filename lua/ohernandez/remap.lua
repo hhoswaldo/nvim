d8a57bd3503dee59ca494548a7ad1475d954d145
oswaldohernandez <oswaldohernandez+workspace@proton.me>
2024-10-13 12:35:50 -0600

    chore: change lsprestart keymap

--- lua/ohernandez/remap.lua
+++ lua/ohernandez/remap.lua
@@ -9,7 +9,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+vim.keymap.set("n", "<leader><F5>", "<cmd>LspRestart<cr>")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
